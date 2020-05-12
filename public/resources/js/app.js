eee9f782a10acce53be32181ff33b6a83c42ab65
Gustavo Tavares <gustavo.tavares2@live.com>
2020-05-11 22:18:07 -0300

    connecting websocket

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -1,17 +1,34 @@
+import PianoWrapper from './pianoWrapper.js';
+
 class AppMain extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = 'Acordes App';
-        this.socket = new WebSocket('ws://localhost:1040');
-        
-        this.socket.onmessage = function(event) {
-            var data = event.data
-            console.log(data);
-        }
+        this.lastPlayedNotes = '';
+        this.lastReleasedNotes = '';
+        this.attachShadow({ mode: 'open'});
 
-        this.socket.onopen = (event) => {
-            this.socket.send('Hello server!')  
-        } 
+        this.socket = new WebSocket('ws://localhost:1040');
+        this.socket.onopen = (event) => this.socket.send('Hello server!');
+    }
+
+    connectedCallback() {
+        this.onpress = new CustomEvent('onpress');
+        this.onrelease = new CustomEvent('onrelease');
+
+        this.socket.onmessage = (event) => {
+            // TODO; check if it is a pressed or released event
+            /**
+             * event.data = { type: 'pressed', notes: [], chord: ''}
+             */
+            this.lastPlayedNotes = event.data;
+            this.dispatchEvent(this.onpress);
+        };
+
+        const piano = new PianoWrapper(12, 'C2');
+        this.shadowRoot.appendChild(piano);
+ 
+        this.addEventListener('onpress', (event) => piano.playNotes(this.lastPlayedNotes));
+        this.addEventListener('onrelease', (event) => piano.releaseNotes(this.lastReleasedNotes));
     }
 }
 
